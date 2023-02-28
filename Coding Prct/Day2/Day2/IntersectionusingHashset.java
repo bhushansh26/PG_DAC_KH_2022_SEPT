0bdc8f142932bd7e1103ae4f535beef0e64c5093
bhushansh26 <113437930+bhushansh26@users.noreply.github.com>
2023-02-28 10:32:47 +0530

    Add files via upload

---
+++ Coding Prct/Day2/Day2/IntersectionusingHashset.java
@@ -0,0 +1,22 @@
+package Day2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class IntersectionusingHashset {
+
+	public static void main(String[] args) {
+		Integer[]a1= {0,1,2,3,4,5};
+		Integer[]a2= {3,4,5,1};
+		
+		HashSet<Integer> s=new HashSet<>();
+		s.addAll(Arrays.asList(a1));
+		s.retainAll(Arrays.asList(a2));
+		
+		System.out.println(s);
+		
+		
+		
+	}
+
+}
